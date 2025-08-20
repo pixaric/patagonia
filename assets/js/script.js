be0b1c82ada8bb7115dd3c3f58f2f4bd18ae4031
reinier <cardosoreinier@gmail.com>
2025-08-20 10:48:58 +0100

    primer
    inicio de todo

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  const navLinks = document.querySelector('.nav-links');
+  navLinks.classList.toggle('active');
+}
+
+function toggleDetails(card) {
+  card.classList.toggle('active');
+}
