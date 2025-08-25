918dc8893eda3c9675b431554e584c0943dad2a7
reinier <reiniercg2020@gmail.com>
2025-08-25 13:23:46 +0100

    a

--- assets/js/pedido copy.js
+++ assets/js/pedido copy.js
@@ -35,31 +35,7 @@ function vaciarPedido() {
   actualizarResumen();
 }
 
-// pop-up
-function mostrarPopup() {
-  document.getElementById("popupOferta").style.display = "flex";
-}
 
-function cerrarPopup() {
-  document.getElementById("popupOferta").style.display = "none";
-}
-
-// Mostrar el popup después de 3 segundos
-setTimeout(mostrarPopup, 3000);
-
-function mostrarFechaHora() {
-  const ahora = new Date();
-  const opciones = {
-    weekday: 'long',
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit'
-  };
-  const formato = ahora.toLocaleString('es-ES', opciones);
-  document.getElementById("fechaHora").textContent = `📅 ${formato}`;
-}
 
 // Mostrar fecha y hora al cargar el ticket
 mostrarFechaHora();
